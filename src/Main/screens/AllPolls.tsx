6460202ef8874bd80fe55176de7ea27c96106fb1
Arun solanki <arunsolanki301@gmail.com>
2022-04-21 16:43:18 +0530

    login signUp

---
+++ src/Main/screens/AllPolls.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AllPolls() {
+  return (
+    <>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+      <div>AllPolls</div>
+    </>
+  );
+}
+
+export default AllPolls;
---
+++ src/Main/screens/CreatePolls.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreatePolls() {
+  return <div>CreatePolls</div>;
+}
+
+export default CreatePolls;
