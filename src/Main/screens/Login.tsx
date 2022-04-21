781b76d756be79186cc22ffc88e605055df3d2ad
Arun solanki <arunsolanki301@gmail.com>
2022-04-21 15:56:13 +0530

    login signUp

--- src/Main/screens/Login.tsx
+++ src/Main/screens/Login.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Spinner } from "react-bootstrap";
 import LabeledInput from "../components/LabeledInput";
 import { Link, useNavigate } from "react-router-dom";
 import "../Styles/LoginStyle.css";
+import axios from "axios";
 
 function Login() {
   const [inputValue, setInputValue] = useState({
@@ -10,7 +11,16 @@ function Login() {
     password: "",
     errorMsg: "",
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const name = localStorage.getItem("user");
+    if (token && name) {
+      navigate("/Home");
+    }
+  }, []);
 
   const onchange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue((prev) => ({
@@ -23,11 +33,30 @@ function Login() {
     event.preventDefault();
     const { email, password } = inputValue;
     if (email && password) {
-      console.log(inputValue, "--------->");
-      navigate("/Home");
+      setIsLoading(true);
+      const Url = `https://secure-refuge-14993.herokuapp.com/login?username=${email}&password=${password}`;
+      axios.post(Url).then((res) => {
+        if (res.data.error == 0) {
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("user", email);
+          navigate("/Home");
+        } else {
+          console.log(res);
+          setInputValue({ ...inputValue, errorMsg: res.data.data });
+          errorMsg();
+        }
+        setIsLoading(false);
+      });
     } else {
       setInputValue({ ...inputValue, errorMsg: "field should not be empty" });
+      errorMsg();
     }
+  };
+
+  const errorMsg = () => {
+    setTimeout(() => {
+      setInputValue({ ...inputValue, errorMsg: "" });
+    }, 5000);
   };
 
   return (
@@ -51,12 +80,15 @@ function Login() {
           onchange={onchange}
         />
         <Button type="submit" className="LoginButton">
-          Login
+          {isLoading ? <Spinner animation="border" size="sm" /> : "Login"}
         </Button>
       </form>
       <Link to="/SignUp" className="LoginAlready">
         new user? sign up instead
       </Link>
+      {inputValue.errorMsg && (
+        <p className="LoginError">{inputValue.errorMsg}</p>
+      )}
     </div>
   );
 }
